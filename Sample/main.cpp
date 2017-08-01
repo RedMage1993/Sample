2f6c0e0f23bfe8fcedf98ad439625a64e80be274
Fritz Ammon <redmage1993@gmail.com>
2017-08-01 12:24:56 -0700

    added a starting point

---
+++ Sample/main.cpp
@@ -0,0 +1,18 @@
+// Fritz Ammon
+//
+// main.cpp
+// 1 August 2017
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hey, how's it going?" << endl;
+
+	cin.get();
+
+	return 0;
+}
