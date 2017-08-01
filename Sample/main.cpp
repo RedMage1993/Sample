19343b9bd3996712df965a8844132045c3bf5b40
Fritz Ammon <redmage1993@gmail.com>
2017-08-01 12:51:09 -0700

    should be nothing in here, actually

--- Sample/main.cpp
+++ Sample/main.cpp
@@ -4,17 +4,9 @@
 // 1 August 2017
 //
 
-#include <iostream>
-
 int main()
 {
-	using std::cout;
-	using std::cin;
-	using std::endl;
-
-	cout << "Hey, how's it going?" << endl;
-
-	cin.get();
+	
 
 	return 0;
 }
